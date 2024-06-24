9941d41d20e2a1632c3af37bf21b88390a08132d
devarslan99 <devarslan99@gmail.com>
2024-06-24 15:23:25 +0500

    add database layer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import express from 'express';
+import { prisma } from './lib/db';
 
 async function serverfunction() {
     const app = express();
@@ -13,20 +14,39 @@ async function serverfunction() {
               hello:String
               say(name:String):String
            }
+         type Mutation{
+          createUser(firstName:String!,lastName:String!,email:String!,password:String!):Boolean
   
+         }
   `,
         resolvers: {
 
-            Query:{
-                hello:()=>`Hi there i am a graphql server`,
-                say:(_,{name}:{name:string})=>`Hi i am ${name}`
+            Query: {
+                hello: () => `Hi there i am a graphql server`,
+                say: (_, { name }: { name: string }) => `Hi i am ${name}`
+            },
+            Mutation: {
+                createUser: async (_, { firstName, lastName, email, password }: { firstName: string, lastName: string, email: string, password: string }) => {
+                    await prisma.user.create({
+                     data:{
+                       email,
+                       firstName,
+                       lastName,
+                       password,
+                       salt:'random-salt'
+                    
+                     }
+
+                    })
+                    return true
+                }
             }
         },
     });
 
     await server.start();
     app.use('/graphql', expressMiddleware(server));
-    app.listen(8000,()=>`server started`)
+    app.listen(8000, () => `server started`)
 }
 
 serverfunction();
